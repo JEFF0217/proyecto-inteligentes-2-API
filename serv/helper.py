12e5960253dfd421b8226f95123cda0d736328dc
JEFF0217 <67935913+JEFF0217@users.noreply.github.com>
2022-12-18 18:21:41 -0500

    Update helper.py

--- serv/helper.py
+++ serv/helper.py
@@ -8,8 +8,9 @@ from io import BytesIO
 import numpy as np
 
 
-clases = ["beachballs", "billiardball",
-              "bowlingball", "football", "golfball", "paintballs", "pokemonballs", "soccerball", "tennisball", "volleyball"]
+clases = ["beachballs", "billiardball", "bowlingball", "football", "golfball",
+          "paintballs", "pokemonballs", "soccerball", "tennisball", "volleyball"]
+
 
 def readb64(base64_string):
     sbuf = BytesIO()
@@ -17,42 +18,42 @@ def readb64(base64_string):
     pimg = Image.open(sbuf)
     return cv2.cvtColor(np.array(pimg), cv2.COLOR_BGR2GRAY)
 
+
 def readb642(image):
 
     decoded_data = base64.b64decode(image)
-    np_data = np.fromstring(decoded_data,np.uint8)
-    img = cv2.imdecode(np_data,cv2.IMREAD_UNCHANGED)
+    np_data = np.fromstring(decoded_data, np.uint8)
+    img = cv2.imdecode(np_data, cv2.IMREAD_UNCHANGED)
     cv2.imshow("test", img)
 
     return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 
-
 def predecir(imagen):
-    
+
     ancho = 128
     alto = 128
     # print(files)
-   
+
     imgageDecoded = readb642(imagen)
-   
-    
+
     pred = Prediccion('modelos/modelo_1v5.h5', ancho, alto)
-  
+
     clase = pred.predecir(imgageDecoded)
     print(clase)
 
     probabilidades = formatPorcentajes(clase["prediccion"])
-    resultados={"clase": clases[clase["clase"]], "probabilidades":probabilidades}
-
+    resultados = {"clase": clases[clase["clase"]],
+                  "probabilidades": probabilidades}
 
     return resultados
 
+
 def formatPorcentajes(predicciones):
-    resultado="Probabilidades: "
-    index=0
+    resultado = "Probabilidades: "
+    index = 0
 
     for prediccion in predicciones:
-        resultado +="\n"+ clases[index] +": " +"{:.4%}".format(prediccion)
-        index+=1
+        resultado += "\n" + clases[index] + ": " + "{:.4%}".format(prediccion)
+        index += 1
     return (resultado)
