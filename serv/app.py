a1b639223bbd890cb57fe4beb04fdfddc09d62b7
JEFF0217 <67935913+JEFF0217@users.noreply.github.com>
2022-12-18 16:31:51 -0500

    init

---
+++ serv/app.py
@@ -0,0 +1,42 @@
+import json
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+from helper import predecir
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return {'user': 'Admin'}
+
+CORS(app)
+cors = CORS(app, resource={
+    r"/*":{
+        "origins":"*"
+    }
+})
+@app.route('/predict', methods=['POST'])
+def predict():
+    if (request.method == 'POST'):
+        data = request.get_json()
+        imagen = data.get("image")
+        predicciones = []
+        predicciones = predecir(imagen)
+        data = {
+            "message": "Predictions made satisfactorily",
+            "clase": predicciones["clase"],
+            "probabilidades": predicciones["probabilidades"]
+
+        }
+        response = app.response_class(response=json.dumps(data),
+                                      status=200,
+                                      mimetype='application/json')
+        return response
+
+
+# We only need this for local development.
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
---
+++ serv/helper.py
@@ -0,0 +1,58 @@
+import os
+from Prediccion import Prediccion
+import base64
+import pathlib
+from PIL import Image
+import cv2
+from io import BytesIO
+import numpy as np
+
+
+clases = ["beachballs", "billiardball",
+              "bowlingball", "football", "golfball", "paintballs", "pokemonballs", "soccerball", "tennisball", "volleyball"]
+
+def readb64(base64_string):
+    sbuf = BytesIO()
+    sbuf.write(base64.b64decode(base64_string))
+    pimg = Image.open(sbuf)
+    return cv2.cvtColor(np.array(pimg), cv2.COLOR_BGR2GRAY)
+
+def readb642(image):
+
+    decoded_data = base64.b64decode(image)
+    np_data = np.fromstring(decoded_data,np.uint8)
+    img = cv2.imdecode(np_data,cv2.IMREAD_UNCHANGED)
+    cv2.imshow("test", img)
+
+    return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+
+def predecir(imagen):
+    
+    ancho = 128
+    alto = 128
+    # print(files)
+   
+    imgageDecoded = readb642(imagen)
+   
+    
+    pred = Prediccion('modelos/modelo_1v5.h5', ancho, alto)
+  
+    clase = pred.predecir(imgageDecoded)
+    print(clase)
+
+    probabilidades = formatPorcentajes(clase["prediccion"])
+    resultados={"clase": clases[clase["clase"]], "probabilidades":probabilidades}
+
+
+    return resultados
+
+def formatPorcentajes(predicciones):
+    resultado="Probabilidades: "
+    index=0
+
+    for prediccion in predicciones:
+        resultado +="\n"+ clases[index] +": " +"{:.4%}".format(prediccion)
+        index+=1
+    return (resultado)
